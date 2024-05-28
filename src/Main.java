40b9adb483e93c9de5fbf2350fac5757e68b554a
Gilllip <filip.kaddik@elev.ga.ntig.se>
2024-05-28 13:26:51 +0200

    Server

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
         if (args.length >= 1){
             portnumber = Integer.parseInt(args[0]);
         }
-        // Create server side socket
+        // Create server side socket k
         try {
             server = new ServerSocket(portnumber);
         } catch (IOException ie){
